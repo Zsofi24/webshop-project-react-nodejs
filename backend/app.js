32266d30a1d5b507f07235e6dc99ebbc954fb141
Zsofi24 <zsofi24.hari.zsofi@gmail.com>
2023-07-06 15:25:22 +0200

    error-handler middleware

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import usersRouter from './routes/users-router.js';
 import productsRouter from './routes/products-router.js';
+import errorHandler from './middlewares/error-handler-middleware.js';
 
 const app = express();
 app.use(express.json());
@@ -10,5 +11,6 @@ app.use(cors());
 app.use('/api', usersRouter);
 app.use('/api', productsRouter);
 
+app.use(errorHandler);
 
 export default app;
