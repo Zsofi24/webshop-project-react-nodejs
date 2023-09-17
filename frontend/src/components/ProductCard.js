7eeeb47cda7c1de2c6a406304863f7a2677e9c8f
Zsofi24 <zsofi24.hari.zsofi@gmail.com>
2023-09-17 15:42:29 +0200

    temporary solution: default img - ProductCard

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -48,7 +48,15 @@ export default function ProductCard({product}) {
     <div className='product-card'>
       <Link to={`${product.id}`}>
       <div className='product-image'>
-        <img src={`${API_URL}/api/${product.path}`} alt="wine" />
+        {/* ideiglenes megoldás!!! default img */}
+        {
+          product.path 
+          ?
+          <img src={`${API_URL}/api/${product.path}`} alt="wine" />
+          :
+          <img src={`${API_URL}/api/uploads/e65d2a0f397b3aadf357c6fd6dde1282-resized`} alt="wine" />
+
+        }
         { product.limited ? <ProductCardLabel text="limitált"/> : null }
       </div>
       <h3 className='product-font-primary'>{product.title}</h3>
