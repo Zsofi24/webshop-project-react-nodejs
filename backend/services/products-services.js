12bd4463e2caa4e557a83232db7f91a1b7e5b214
Zsofi24 <zsofi24.hari.zsofi@gmail.com>
2023-10-03 16:17:22 +0200

    reduce async edit function

--- backend/services/products-services.js
+++ backend/services/products-services.js
@@ -18,18 +18,22 @@ export default {
     // imgupload(newPath, productid) {
     //     return productsModel.imgupload(newPath, productid)
     // },
+
     getAll() {
         return productsModel.getAll()
     },
+
     getCurrent({ pageSize, currentPage, sortBy, order, filter, products }) {
         if(!Array.isArray(filter) && filter) filter = [filter];
         if(products == "all") products = -1
         else products = 0
         return productsModel.getCurrent({ pageSize, currentPage, sortBy, order, filter, products })
     },
+    
     getOne({ productid }) {
         return productsModel.getOne({ productid })
     },
+
     async edit({ title, price, description, id, categories, stock, visible, path, limited }) {
         visible = JSON.parse(visible);
         limited = JSON.parse(limited);
@@ -39,17 +43,14 @@ export default {
            path = `${path}-resized`
         }
 
+        await productsCategoriesModel.delete(id)
         if(categories) {
-            const resp = await productsCategoriesModel.delete(id)
-            const resp2 = await productsCategoriesModel.setToProduct(id, categories)
-            const productsResp = await productsModel.edit({ title, price, description, id, stock, visible, path, limited })
-            return productsResp                 
-        } else {
-            const resp = await productsCategoriesModel.delete(id)
-            const productsResp = await productsModel.edit({ title, price, description, id, stock, visible, path, limited })
-            return productsResp               
+           await productsCategoriesModel.setToProduct(id, categories)
         }
+        const productsResp = await productsModel.edit({ title, price, description, id, stock, visible, path, limited })
+        return productsResp               
     },
+
     delete(id) {
         return productsModel.delete(id)
     }
