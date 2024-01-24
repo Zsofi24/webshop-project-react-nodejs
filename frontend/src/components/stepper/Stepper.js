be21d69006ac2311a431d0dadcd596a86357baa6
Zsofi24 <zsofi24.hari.zsofi@gmail.com>
2024-01-24 15:12:30 +0100

    stepper - change classnames( BEM )

--- frontend/src/components/stepper/Stepper.js
+++ frontend/src/components/stepper/Stepper.js
@@ -1,32 +1,26 @@
-import { Fragment } from "react";
 import { StyledStepperCircle, StyledStepperLine } from "./StyledStepper";
 
-export default function Stepper ({currentStep}) {
+export default function Stepper ({ currentStep }) {
 
   const numberOfSteps= 3;
   const labels = ["Kosár", "Adatok", "Összesítés"];
-  // const isFinalStep = (index) => index === numberOfSteps - 1;
-  // const isFirstStep = (index) => index === 0;
-  const isActive = (index) => currentStep >= index ? true : false
-  const isActiveLine = (index) => currentStep >= index +1 ? true : false
+  const isActive = (index) => currentStep >= index;
+  const isActiveLine = (index) => currentStep >= index + 1; 
 
   return (
-    <div className="stepper-wrapper">
-      <div className="stepper-container">
+    <div className="stepper">
+      <div className="stepper__container">
         {Array.from({length: numberOfSteps}).map((_, index) => (
-          <div className="stepper-step" key={index}>
-          <div className="stepper-show">
-            {/* {isFirstStep(index) ? null : <StyledStepperLine active={isActive(index)} />} */}
-            <StyledStepperLine active={isActive(index)} hide={index == 0}/>
-            <StyledStepperCircle active={isActive(index)}/>
-            <StyledStepperLine active={isActiveLine(index)} hide={index == numberOfSteps -1}/>
-            {/* {isFinalStep(index) ? null : <StyledStepperLine active={isActive(index)} />} */}
+          <div className="stepper__step" key={index}>
+            <div className="stepper__progress">
+              <StyledStepperLine active={isActive(index)} hide={index == 0}/>
+              <StyledStepperCircle active={isActive(index)}/>
+              <StyledStepperLine active={isActiveLine(index)} hide={index == numberOfSteps -1}/>
+            </div>
+            <div className="stepper__labels">
+                {labels[index]}
+            </div>
           </div>
-          <div className="stepper-labels">
-              {labels[index]}
-          </div>
-          </div>
-
         ))}
       </div>
     </div>
